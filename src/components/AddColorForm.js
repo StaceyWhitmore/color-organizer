48f0f9dd09b3012d212f11cf7c06f4e46ff81bd9
StaceyWhitmore <staceywhitmore@gmail.com>
2018-10-17 18:19:39 -0600

    added constructor and submit function to component

--- src/components/AddColorForm.js
+++ src/components/AddColorForm.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react'
 
 class AddColorForm extends Component {
+  constructor(props) {
+    super(props)
+    this.submit = this.submit.bind(this)
+  }
+  submit(e) {
+    const {_title, _color, } = this.refs
+    e.preventDefault()
+    alert(`New Color: ${_title.value} ${_color.value}`)
+    _title.value = ''
+    _color.value = '#000000'
+    _title.focus()
+  }
 
 render() {
   return (
-    <form onSubmit={e=>e.preventDefault()}>
-      <input
-            type="text"
-            placeholder="color title..." required/>
-      <input 
+    <form onSubmit={this.submit}>
+      <input ref="_title"
+             type="text"
+             placeholder="color title..." required/>
+      <input ref="_color"
              type="color" required/>
       <button>+</button>
     </form>
