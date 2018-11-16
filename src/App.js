465ce9d18dac401751aa5e87d770088b0bb4ce3f
StaceyWhitmore <staceywhitmore@gmail.com>
2018-11-15 18:32:57 -0700

    minor adjustments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //import { render } from 'react-dom'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Menu from './ui/Menu'
 import Page404 from './ui/Page404'
 import { Colors, Color, NewColor } from './containers.js'
@@ -22,7 +22,7 @@ const App = () =>
     )} />
   </Switch>
 
-/*
+/* see index.js
 render(
     <Provider store={store}>
       <HashRouter>
