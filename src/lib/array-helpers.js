4a087cfefde434eca7ff1df6f7847452d3bfd915
StaceyWhitmore <staceywhitmore@gmail.com>
2018-11-15 17:19:04 -0700

    filled helper fns stubbed earlier in array-helpers.js

--- src/lib/array-helpers.js
+++ src/lib/array-helpers.js
@@ -1,5 +1,6 @@
 import { compopse } from 'redux'
 
+/*
 const sortBy = (type, field) => {
   switch (type) {
     case "date": //if sort by field timestamp(of type: date) then ...
@@ -14,6 +15,7 @@ const sortBy = (type, field) => {
       return (a,b) => b[field] - a[field] //...other by field rating ( of type: number)
   }//close switch
 }//close <sortBy() />
+*/
 
 const sortByDate = () =>
   (a,b) => new Date(b[field]) - new Date(a[field])
@@ -23,11 +25,17 @@ const sortByString = () =>
  1 //...otherwise, truthy(1)
 
 //rating
-const sortByNumber
+const sortByNumber = () =>
+  (a,b) => b[field] - a[field]
 
-const whichSort
+const whichSort = () =>
+(type === "date") ?
+      sortByDate(field) :
+      (type === "string") ?
+          sortByString(field) :
+          sortByNumber(field)
 
-
+//EXPORTED returns sortBy("type", "by")
 export const sortFunction = sort =>
   (sort === "SORTED_BY_TITLE") ?
     sortBy("string","title") : //by field title (of type: string) type (a) = "string", field (b) = "title"
@@ -35,22 +43,38 @@ export const sortFunction = sort =>
       sortBy("number","rating") : //by field rating(of type: number)
       sortBy("date","timestamp")  //...otherwise, by field timestamp (of type: date)
 
+// returned in stateHash["date"] = "SORTED_BY_DATE" (eventually passed to sortFn above)
+cont getSortState = (sortBy= "date",
+  stateHash= {
+    date: "SORTED_BY_DATE",
+    title: "SORTED_BY_TITLE",
+    rating: "SORTED_BY_RATING"
+  }) => stateHash[sortBY]
 
-cont getSortState
+//returns sortBy("type", "by")
+const locateSortFunction = () => compose(
+  sortFunction,
+  getSortState
+)
 
-const locateSortFunction
 
+//** MAIN EXPORTED
+export const sortColors = (colors, sortBy) =>
+  compose(
+    fn => [...colors].sort(fn),
+    locateSortFunction
+  )(sortBy) //invoked with sortBy() returned from locateSortFunction() -->fn [...colors] will be sorted by
 
-const sortColors
-
+  //EXPORTED to compose findById()
 export const getFirstItem = () =>
   array => array[0]
 
+  //EXPORTED to compose findById()
 export const filterById = (array, id) =>
   array.filter(item=>item.id === id) //filter out the item with matching id and put in into array at [0]
 
-//connect runs functions from R -> L (instead of Left to Right)
-export const findById = connect(
+//**MAIN EXPORT connect runs functions from R -> L (instead of Left to Right)
+export const findById = compose(
   getFirstItem,
   filterById
 )
