6fc539ddcb815bf222d6aa6219c1f282506fe736
StaceyWhitmore <staceywhitmore@gmail.com>
2018-11-16 10:15:49 -0700

    removed sortMenu

--- src/actions.js
+++ src/actions.js
@@ -1,22 +1,24 @@
 import C from './constants'
 import { v4 } from 'uuid'
 
-//addColor(), removeColor(), rateColor(), sortColors()
-export const addColor = (title, color) => ({
-  type: C.ADD_COLOR,
-  id: v4(),
-  title, // --> title: title ..?
-  color, // --> color: color //?
-  timestamp: new Date().toString()
-})
+export const addColor = (title, color) =>
+    ({
+        type: C.ADD_COLOR,
+        id: v4(),
+        title,
+        color,
+        timestamp: new Date().toString()
+    })
 
-export const removeColor = (id) => ({
-  type:C.REMOVE_COLOR,
-  id    // --> id: id  (id of color to remove)
-})
+export const removeColor = id =>
+    ({
+        type: C.REMOVE_COLOR,
+        id
+    })
 
-export const rateColor = (id, rating) => ({
-  type: C.RATE_COLOR,
-  id,    //id: id (...of color to rate)
-  rating //rating: rating (_ of 5 stars)
-})
+export const rateColor = (id, rating) =>
+    ({
+        type: C.RATE_COLOR,
+        id,
+        rating
+    })
---
+++ src/components/ui/Menu.js
@@ -0,0 +1,20 @@
+/*upper menu for sorting by: date(default), title, or rating*/
+import PropTypes from 'prop-types'
+import { NavLink }  from 'react-router-dom'//'react-router'
+import '../../stylesheets/Menu.scss'
+
+const selectedStyle = { color: 'red' }
+
+
+const Menu = ({ match }) =>
+  <nav className="menu">
+    <NavLink to="/" style={match.isExact && selectedStyle}> date </NavLink>
+    <NavLink to="/sort/title" activeStyle={selectedStyle}>title</NavLink>
+    <NavLink to="/sort/rating" activeStyle={selectedStyle}>rating</NavLink>
+  </nav>
+
+  Menu.propTypes = {
+    sort: PropTypes.string
+  }
+
+export default Menu
