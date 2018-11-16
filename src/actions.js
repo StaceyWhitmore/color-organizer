6932f357d13a79d3cf51c2e758a8dd31f301d594
StaceyWhitmore <staceywhitmore@gmail.com>
2018-11-15 19:47:11 -0700

    cleaned up actions

--- src/actions.js
+++ src/actions.js
@@ -1,28 +1,24 @@
-//Now using react-router 4.0
+
 import C from './constants'
 import { v4 } from 'uuid'
 
-//addColor(), removeColor(), rateColor(), sortColors()
-export const addColor = (title, color) => ({
-  type: C.ADD_COLOR,
-  id: v4(),
-  title, // --> title: title ..?
-  color, // --> color: color //?
-  timestamp: new Date().toString()
-})
+export const addColor = (title, color) =>
+    ({
+        type: C.ADD_COLOR,
+        id: v4(),
+        title,
+        color,
+        timestamp: new Date().toString()
+    })
 
-export const removeColor = (id) => ({
-  type:C.REMOVE_COLOR,
-  id    // --> id: id  (id of color to remove)
-})
+export const removeColor = id =>
+    ({
+        type: C.REMOVE_COLOR,
+        id
+    })
 
-export const rateColor = (id, rating) => ({
-  type: C.RATE_COLOR,
-  id,    //id: id (...of color to rate)
-  rating //rating: rating (_ of 5 stars)
-})
-
-export const sortColors = (sortBy="SORTED_BY_DATE") => ({
-  type: C.SORT_COLORS,
-  sortBy
-})
+export const rateColor = (id, rating) =>
+    ({
+        type: C.RATE_COLOR,
+        id,
+        rating
